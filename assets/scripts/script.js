79e065d5bf658e7d26874774f9b512bad550deae
JacksonHilton77 <jacksonthilton@gmail.com>
2023-06-14 19:43:21 -0700

    Test for MusixMatch results

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,7 +9,7 @@
 // // var searchParameters = {
 // //     title: // fetch required
 // //     artist: // fetch required
-    
+
 // // }
 
 // // var pushToStorage = [];
@@ -23,12 +23,12 @@
 
 // searchButton.addEventListener("click", function(event) {
 //     event.preventDefault();
-    
+
 //     searches.push(searchText.value);
 //     searchText.value = "";
 
 //     localStorage.setItem("search", JSON.stringify(searches));
-    
+
 //     // addSearchHistory();
 //     // findCover();
 // })
@@ -43,7 +43,7 @@
 // // function addSearchHistory() {
 // //     // save to local storage
 // //     console.log(searches);
-    
+
 // //     for (var i = 0; i < searches.length; i++) {
 // //         console.log(searches[i]);
 // //     }
@@ -58,36 +58,66 @@
 /* Jackson's area */
 
 //musixmatch track search api query string https://api.musixmatch.com/ws/1.1/track.search?q_track=thunderstruck&apikey=1bea274e43466310a83604d5c9dffd24
+
 var searchInput = document.getElementById("search-input");
 var searchButton = document.getElementById("search-button");
-var resultsContainer = document.getElementById("results-container");
 
 searchButton.addEventListener("click", performSearch);
 
+// function performSearch(event) {
+//   event.preventDefault();
+//   var searchTerm = searchInput.value;
+//   console.log(searchTerm);
+//   var apiUrl = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${encodeURIComponent(searchTerm)}&apikey=1bea274e43466310a83604d5c9dffd24`;
+
+//   fetch(apiUrl)
+//   .then((response) => response.json())
+//   .then((data) => displayResults(data))
+//   .catch((error) => console.log(error));
+
+//   console.log(apiUrl);
+// }
+
 function performSearch(event) {
+  //testing function
   event.preventDefault();
-  var searchTerm = searchInput.value;
-  console.log(searchTerm);
-  var apiUrl = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${encodeURIComponent(searchTerm)}&apikey=1bea274e43466310a83604d5c9dffd24`;
+  var apiUrl = "./assets/json/musixMatch.json";
 
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => displayResults(data))
     .catch((error) => console.log(error));
-
-  console.log(apiUrl);
 }
+
+var seeData ="";
 
 function displayResults(data) {
-  resultsContainer.innerHTML = "";
+  var resultData = data.message.body.track_list;
+  console.log(resultData);
+  seeData = resultData;
+  var resultsContainer = document.querySelector(".hero-section");
+  console.log(resultsContainer);
+  var hideHolder = document.querySelector(".hero-section-text");
+  console.log(hideHolder);
+  hideHolder.style.display = "none";
 
-  data.forEach((result) => {
-    var resultItem = document.createElement("div");
-    resultItem.textContent = result.title;
-    resultsContainer.appendChild(resultItem);
-  });
+  for (let i = 0; i < resultData.length; i++) {
+    const element = resultData[i];
+    var newBox = document.createElement("div");
+    newBox.textContent = element.track.track_name;
+    resultsContainer.appendChild(newBox);
+  }
+
+
+  // Set content or modify other attributes of the new box element
+  // resultsContainer.textContent = resultData;
+  // resultsContainer.style.backgroundColor = "lightblue";
+  // resultsContainer.style.padding = "10px";
+
+  // Append the new box element to the results container
+  // resultsContainer.appendChild(resultsContainer);
 }
-
+console.log(seeData);
 /* Jackson's area */
 
 /* Lidell's area */
@@ -97,4 +127,3 @@ function displayResults(data) {
 /* Jesus' area */
 
 /* Jesus' area */
-
