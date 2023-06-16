39b1ebd466231d963361085cc8a8dbae0802ec80
Jesus Santos <jo_nez@hotmail.com>
2023-06-16 15:07:42 -0700

    result display render complete

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -34,88 +34,90 @@ function displayTrackSearchResults(trackSearchData) {
   trackSearchDisplay.style.display = 'none';
 
   trackSearchResultsContainer.innerHTML = "";
-  
+
   for (let i = 0; i < trackSearchData.message.body.track_list.length; i++) {
     var trackIndex = trackSearchData.message.body.track_list[i];
     var trackId = trackIndex.track.track_id;
     var trackName = trackIndex.track.track_name;
-    var trackAlbumId = trackIndex.track.album_id;
-    var trackAlbumName = trackIndex.track.album_name;
-    var trackArtistId = trackIndex.track.artist_id;
-    var trackArtistName = trackIndex.track.artist_name;
+    // var albumId = trackIndex.track.album_id;
+    var albumName = trackIndex.track.album_name;
+    // var artistId = trackIndex.track.artist_id;
+    var artistName = trackIndex.track.artist_name;
 
     var trackSearchResultsItem = document.createElement('div');
-    trackSearchResultsItem.setAttribute('id', 'track-'+i);
+    trackSearchResultsItem.setAttribute('id', trackId + '-container');
     trackSearchResultsItem.setAttribute('class', 'card-user-container');
-    
-    var trackSearchResultsItemAlbumArtContainer = document.createElement('div');
-    trackSearchResultsItemAlbumArtContainer.setAttribute('id', 'album-art-container-'+i);
-    trackSearchResultsItemAlbumArtContainer.setAttribute('class', 'card-user-avatar');
-    
-    var trackSearchResultsItemAlbumArt = document.createElement('img');
-    trackSearchResultsItemAlbumArt.setAttribute('id', 'album-art-'+i);
-    trackSearchResultsItemAlbumArt.setAttribute('src', 'https://placehold.it/350x350');
-    trackSearchResultsItemAlbumArt.setAttribute('alt', 'Track '+i+' album cover art.');
-    trackSearchResultsItemAlbumArt.setAttribute('class', 'user-image');
+
+    var trackAlbumArtContainer = document.createElement('div');
+    trackAlbumArtContainer.setAttribute('id', trackId + '-art-container');
+    trackAlbumArtContainer.setAttribute('class', 'card-user-avatar');
+
+    var trackAlbumArt = document.createElement('img');
+    trackAlbumArt.setAttribute('id', trackId + '-album-art');
+    trackAlbumArt.setAttribute('src', 'https://via.placeholder.com/350x350');
+    trackAlbumArt.setAttribute('alt', albumName + ' cover art.');
+    trackAlbumArt.setAttribute('class', 'user-image');
 
 
-    trackSearchResultsItemAlbumArtContainer.appendChild(trackSearchResultsItemAlbumArt);
-    trackSearchResultsItem.appendChild(trackSearchResultsItemAlbumArtContainer);
-    
-    var trackSearchResultsItemLinksContainer = document.createElement('div');
-    trackSearchResultsItemLinksContainer.setAttribute('id', 'album-links-'+i);
-    trackSearchResultsItemLinksContainer.setAttribute('class', 'card-user-social');
-    
-    var trackSearchResultsItemLinksList = document.createElement('ul');
-    trackSearchResultsItemLinksList.setAttribute('id', 'album-links-list-'+i);
-    trackSearchResultsItemLinksList.setAttribute('class', 'menu');
-    
-    var trackSearchResultsItemLinksListLyrics = document.createElement('li');
-    trackSearchResultsItemLinksListLyrics.setAttribute('id', 'track-lyrics-'+i);
-    trackSearchResultsItemLinksListLyrics.setAttribute('class', 'fa-solid fa-microphone-lines fa-2xl');
-    
-    var trackSearchResultsItemLinksListWatch = document.createElement('li');
-    trackSearchResultsItemLinksListWatch.setAttribute('id', 'track-watch-'+i);
-    trackSearchResultsItemLinksListWatch.setAttribute('class', 'fa-brands fa-youtube fa-2xl');
-    
-    var trackSearchResultsItemLinksListListen = document.createElement('li');
-    trackSearchResultsItemLinksListListen.setAttribute('id', 'track-listen-'+i);
-    trackSearchResultsItemLinksListListen.setAttribute('class', 'fa-brands fa-spotify fa-2xl');
-    
-    var trackSearchResultsItemLinksListBuy = document.createElement('li');
-    trackSearchResultsItemLinksListBuy.setAttribute('id', 'track-buy-'+i);
-    trackSearchResultsItemLinksListBuy.setAttribute('class', 'fa-brands fa-spotify fa-2xl');
-    
-    trackSearchResultsItemLinksList.appendChild(trackSearchResultsItemLinksListLyrics);
-    trackSearchResultsItemLinksList.appendChild(trackSearchResultsItemLinksListWatch);
-    trackSearchResultsItemLinksList.appendChild(trackSearchResultsItemLinksListListen);
-    trackSearchResultsItemLinksList.appendChild(trackSearchResultsItemLinksListBuy);
-    trackSearchResultsItemLinksContainer.appendChild(trackSearchResultsItemLinksList);
-    trackSearchResultsItem.appendChild(trackSearchResultsItemLinksContainer);
+    trackAlbumArtContainer.appendChild(trackAlbumArt);
+    trackSearchResultsItem.appendChild(trackAlbumArtContainer);
 
-    var trackSearchResultsItemTitles = document.createElement('div');
-    trackSearchResultsItemTitles.setAttribute('id', 'track-info-'+i);
-    trackSearchResultsItemTitles.setAttribute('class', 'card-user-bio');
-    
-    var trackSearchResultsItemTitlesArtist = document.createElement('h4');
-    trackSearchResultsItemTitlesArtist.setAttribute('id', 'track-artist-'+i);
-    
-    var trackSearchResultsItemTitlesTrack = document.createElement('p');
-    trackSearchResultsItemTitlesTrack.setAttribute('id', 'track-title-'+i);
-    
-    trackSearchResultsItemTitles.appendChild(trackSearchResultsItemTitlesArtist);
-    trackSearchResultsItemTitles.appendChild(trackSearchResultsItemTitlesTrack);
-    trackSearchResultsItem.appendChild(trackSearchResultsItemTitles);
-    
-}
+    var trackLinksContainer = document.createElement('div');
+    trackLinksContainer.setAttribute('id', trackId + '-album-links');
+    trackLinksContainer.setAttribute('class', 'card-user-social');
 
-    // '<h4 id="album-artist">AC/DC</h4>' +
-    // '<p id="album-track">Thunderstruck</p>' +
-    // '<span class="location"><span class="location-icon fa fa-map-marker"></span><span class="location-text">Makkah Al-Mukaramah</span></span>' +
-    // '</div>' +
-    // '<div class="card-user-button">' +
-    // '<a href="#" class="button">FOLLOW</a>' +
-    // '</div>'
+    var trackLinksListContainer = document.createElement('ul');
+    trackLinksListContainer.setAttribute('id', trackId + '-album-links-list');
+    trackLinksListContainer.setAttribute('class', 'menu');
+
+    var trackLinksListLyrics = document.createElement('li');
+    trackLinksListLyrics.setAttribute('id', trackId + '-track-lyrics');
+    trackLinksListLyrics.setAttribute('class', 'fa-solid fa-microphone-lines fa-2xl');
+
+    var trackLinksListWatch = document.createElement('li');
+    trackLinksListWatch.setAttribute('id', trackId + '-track-watch');
+    trackLinksListWatch.setAttribute('class', 'fa-brands fa-youtube fa-2xl');
+
+    var trackLinksListListen = document.createElement('li');
+    trackLinksListListen.setAttribute('id', trackId + '-track-listen');
+    trackLinksListListen.setAttribute('class', 'fa-brands fa-spotify fa-2xl');
+
+    var trackLinksListBuy = document.createElement('li');
+    trackLinksListBuy.setAttribute('id', trackId + '-track-buy');
+    trackLinksListBuy.setAttribute('class', 'fa-brands fa-amazon fa-2xl');
+
+    trackLinksListContainer.appendChild(trackLinksListLyrics);
+    trackLinksListContainer.appendChild(trackLinksListWatch);
+    trackLinksListContainer.appendChild(trackLinksListListen);
+    trackLinksListContainer.appendChild(trackLinksListBuy);
+    trackLinksContainer.appendChild(trackLinksListContainer);
+    trackSearchResultsItem.appendChild(trackLinksContainer);
+
+    var trackTitlesContainer = document.createElement('div');
+    trackTitlesContainer.setAttribute('id', trackId + '-track-info-container');
+    trackTitlesContainer.setAttribute('class', 'card-user-bio');
+
+    var trackTitlesArtist = document.createElement('h4');
+    trackTitlesArtist.setAttribute('id', trackId + '-track-artist');
+    trackTitlesArtist.textContent = artistName;
+
+    var trackTitlesTrack = document.createElement('p');
+    trackTitlesTrack.setAttribute('id', trackId + '-track-title');
+    trackTitlesTrack.textContent = trackName;
+
+    trackTitlesContainer.appendChild(trackTitlesArtist);
+    trackTitlesContainer.appendChild(trackTitlesTrack);
+    trackSearchResultsItem.appendChild(trackTitlesContainer);
+
+  }
+
+  // '<h4 id="album-artist">AC/DC</h4>' +
+  // '<p id="album-track">Thunderstruck</p>' +
+  // '<span class="location"><span class="location-icon fa fa-map-marker"></span><span class="location-text">Makkah Al-Mukaramah</span></span>' +
+  // '</div>' +
+  // '<div class="card-user-button">' +
+  // '<a href="#" class="button">FOLLOW</a>' +
+  // '</div>'
 
 
   // trackSearchData.body.track_list.forEach((result) => {
@@ -124,6 +126,9 @@ function displayTrackSearchResults(trackSearchData) {
   //   resultItem.textContent = result.title;
   //   resultsContainer.appendChild(resultItem);
   // });
+
+  trackSearchResultsContainer.appendChild(trackSearchResultsItem);
+
   trackSearchResultsContainer.style.display = 'block';
 }
 
