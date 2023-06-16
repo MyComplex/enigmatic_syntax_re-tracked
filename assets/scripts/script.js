ba8079da4310f87669ad96eed4458712c759829e
Jason <jasongabs@gmail.com>
2023-06-15 21:19:50 -0700

    Added Clear functionality with the local storage integration

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,44 +2,43 @@
 
 var searchButton = document.querySelector("#searchButton");
 var searchText = document.querySelector("#searchText");
-var clearHistory = document.querySelector("#clearHistory")
-
-var searchedItem;
-var userSearches;
-
-// var searchParameters = {
-//     title: // fetch required
-//     artist: // fetch required
-    
-// }
-
-// var pushToStorage = [];
-
-// function findCover() {
-    // add fetch here
-
-// }
+var clearHistory = document.querySelector("#clearHistory");
+var searchLi = document.querySelector("#li");
+var listItems = document.querySelector("#listItem");
 
 var searches = [];
+
+var searchString = null;
 
 searchButton.addEventListener("click", function(event) {
     event.preventDefault();
     
     searches.push(searchText.value);
+    searchString = searchText.value;
+    showOneSearchAtATime();
+
     searchText.value = "";
 
     localStorage.setItem("search", JSON.stringify(searches));
-    searches = localStorage.getItem('search');
-    showSearches(searches);
-    // addSearchHistory();
-    // findCover();
+    showHistory();
 })
-clearHistory.addEventListener("click", function(event) {localStorage.clear()})
 
-function showSearches() {
+clearHistory.addEventListener("click", function(event) {
+    localStorage.clear();
+    searches = [];
+    listItems.innerHTML = "";
+})
+
+function showHistory() {
+  if (searchLi) {
+    searchLi.innerHTML = "";
+  }  
+}
+
+function showSearchesArrayOnReload() {
   for (var i = 0; i < searches.length; i++) {
     var element = searches[i];
-
+    
     var listContainer = document.getElementById("listItem");
     var listItem = document.createElement("li");
 
@@ -49,37 +48,33 @@ function showSearches() {
   }
 }
 
+function showOneSearchAtATime() {
+    var element = searchString;
 
+    var listContainer = document.getElementById("listItem");
+    var listItem = document.createElement("li");
 
-// clearHistory.addEventListener("click", clearButtons());
+    listItem.setAttribute("id", 0);
+    listItem.textContent = element;
+    listContainer.appendChild(listItem);
+
+}
 
 function init () {
     var searchesList = JSON.parse(localStorage.getItem("search"));
     if (searchesList !== null) {
         searches = searchesList;
+        showSearchesArrayOnReload(searches);
     }
 }
 
-// function clearButtons(event) {
-//   event.preventDefault();
-//   searches = [];
-//   var listContainer = document.getElementById("listItem");
-//   listContainer.innerHTML = "";
-
-// }
-
-// function addSearchHistory() {
-//     // save to local storage
-//     console.log(searches);
-    
-//     for (var i = 0; i < searches.length; i++) {
-//         console.log(searches[i]);
-//     }
-//     // userSearches.textContent = searches;
-//     // userSearches is the list of searches to display while searches is an array of them
-// }
-
 init();
+
+
+
+
+
+
 
 /* Jason's area */
 
