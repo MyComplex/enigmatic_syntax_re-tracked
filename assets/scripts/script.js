f0986c8fa1c6b29963cbaaa9524d308f5433bf6b
Jesus Santos <jo_nez@hotmail.com>
2023-06-18 00:10:59 -0700

    more work on results slider

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
-/* Code to run */
 /* GLOBAL VARIABLES */
 var apiKey = '1bea274e43466310a83604d5c9dffd24';
 var historyContainer = document.getElementById('history-container');
 var trackSearchDisplay = document.getElementById('main-container');
 var searchText = document.getElementById('search-text');
 var searchButton = document.getElementById('search-button');
-var trackSearchResultsContainer = document.getElementById('results-container');
-var trackSearchResultsContainerPaginator = document.getElementById('results-paginator');
+var sliderContainer = document.getElementById('results');
+var trackSearchResultsCardContainer = document.getElementById('results-card-container');
+var trackSearchResultsCardContainerPaginator = document.getElementById('results-paginator');
 
 var searches = localStorage.getItem('searches');
 searches = (searches) ? JSON.parse(searches) : [];
@@ -35,25 +35,7 @@ function displayTrackSearchResults(trackSearchData) {
 /* HIDE APP TITLE & SEARCH BAR */
   trackSearchDisplay.style.display = 'none';
 /* CLEAR RESULTS CONTAINER */
-  trackSearchResultsContainer.innerHTML = "";
-
-/* BUILD RESULTS HEADER
-var resultsHeaderContainer = document.createElement('div');
-// var resultsHeaderArtistAndTitleContainer = document.createElement('div');
-var resultsHeaderArtistAndTitle = document.createElement('div');
-resultsHeaderArtistAndTitle.textContent = 'Artist & Track Title';
-// var resultsHeaderAlbumArtContainer = document.createElement('div');
-var resultsHeaderAlbumArt = document.createElement('div');
-resultsHeaderAlbumArt.textContent = 'Album Cover';
-// var resultsHeaderLinksContainer = document.createElement('div');
-var resultsHeaderLinks = document.createElement('div');
-resultsHeaderLinks.textContent = 'Additional Track Options';
-
-resultsHeaderContainer.appendChild(resultsHeaderArtistAndTitle);
-resultsHeaderContainer.appendChild(resultsHeaderAlbumArt);
-resultsHeaderContainer.appendChild(resultsHeaderLinks);
-trackSearchResultsContainer.appendChild(resultsHeaderContainer);
- */
+  trackSearchResultsCardContainer.innerHTML = "";
 
 /* LOOP THROUGH RESULTS */
   for (let i = 0; i < trackSearchData.message.body.track_list.length; i++) {
@@ -132,340 +114,13 @@ trackSearchResultsContainer.appendChild(resultsHeaderContainer);
     trackSearchResultsItem.appendChild(trackAlbumArtContainer);
     trackSearchResultsItem.appendChild(trackLinksContainer);
     
-    trackSearchResultsContainer.appendChild(trackSearchResultsItem);
+    trackSearchResultsCardContainer.appendChild(trackSearchResultsItem);
   }
-  
-  // '<h4 id="album-artist">AC/DC</h4>' +
-  // '<p id="album-track">Thunderstruck</p>' +
-  // '<span class="location"><span class="location-icon fa fa-map-marker"></span><span class="location-text">Makkah Al-Mukaramah</span></span>' +
-  // '</div>' +
-  // '<div class="card-user-button">' +
-  // '<a href="#" class="button">FOLLOW</a>' +
-  // '</div>'
 
-
-  // trackSearchData.body.track_list.forEach((result) => {
-  //   trackSearchResultsContainer.appendChild(trackSearchResultsItem);
-  //   var trackLyricsIcon = document.querySelector('.fa-microphone-lines');
-  //   resultItem.textContent = result.title;
-  //   resultsContainer.appendChild(resultItem);
-  // });
-
-
-  trackSearchResultsContainer.style.display = 'flex';
+  sliderContainer.style.display = 'block';
 }
 
-/* Code to run */
-
-/* Jason's area */
-/* Jason's area */
-
-/* Jackson's area */
-
-// var searchInput = document.getElementById("search-input");
-// var searchButton = document.getElementById("search-button");
-// var resultsContainer = document.getElementById("results-container");
-
-// searchButton.addEventListener("click", performSearch);
-
-// function performSearch(event) {
-//     event.preventDefault();
-//     var searchTerm = searchInput.value;
-//     console.log(searchTerm);
-//     var apiUrl = `https://api.publicapis.org/entries=${encodeURIComponent(searchTerm)}`;
-
-//     fetch(apiUrl, {
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//     })
-//         .then((response) => response.json())
-//         .then((data) => displayResults(data))
-//         .catch((error) => console.log(error));
-
-//     console.log(apiUrl);
-// }
-
-// function displayResults(data) {
-//     resultsContainer.innerHTML = "";
-
-//     data.forEach((result) => {
-//         var resultItem = document.createElement("div");
-//         resultItem.textContent = result.title;
-//         resultsContainer.appendChild(resultItem);
-//     });
-// }
-
-/* Jackson's area */
-
-// /* Lidell's area */
-
-// const APIController = (function () {
-
-//     const clientId = '07ff8b4538b94722848066ac1547ceb1';
-//     const clientSecret = 'e29634d15e1d4ac4bf533209bcc77367';
-
-//     // private methods
-//     const _getToken = async () => {
-
-//         const result = await fetch('https://accounts.spotify.com/api/token', {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded',
-//                 'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-//             },
-//             body: 'grant_type=client_credentials'
-//         });
-
-//         const data = await result.json();
-//         return data.access_token;
-//     }
-
-//     const _getGenres = async (token) => {
-
-//         const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
-//             method: 'GET',
-//             headers: { 'Authorization': 'Bearer ' + token }
-//         });
-
-//         const data = await result.json();
-//         return data.categories.items;
-//     }
-
-//     const _getPlaylistByGenre = async (token, genreId) => {
-
-//         const limit = 10;
-
-//         const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
-//             method: 'GET',
-//             headers: { 'Authorization': 'Bearer ' + token }
-//         });
-
-//         const data = await result.json();
-//         return data.playlists.items;
-//     }
-
-//     const _getTracks = async (token, tracksEndPoint) => {
-
-//         const limit = 10;
-
-//         const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-//             method: 'GET',
-//             headers: { 'Authorization': 'Bearer ' + token }
-//         });
-
-//         const data = await result.json();
-//         return data.items;
-//     }
-
-//     const _getTrack = async (token, trackEndPoint) => {
-
-//         const result = await fetch(`${trackEndPoint}`, {
-//             method: 'GET',
-//             headers: { 'Authorization': 'Bearer ' + token }
-//         });
-
-//         const data = await result.json();
-//         return data;
-//     }
-
-//     return {
-//         getToken() {
-//             return _getToken();
-//         },
-//         getGenres(token) {
-//             return _getGenres(token);
-//         },
-//         getPlaylistByGenre(token, genreId) {
-//             return _getPlaylistByGenre(token, genreId);
-//         },
-//         getTracks(token, tracksEndPoint) {
-//             return _getTracks(token, tracksEndPoint);
-//         },
-//         getTrack(token, trackEndPoint) {
-//             return _getTrack(token, trackEndPoint);
-//         }
-//     }
-// })();
-
-
-// // UI Module
-// const UIController = (function () {
-
-//     //object to hold references to html selectors
-//     const DOMElements = {
-//         selectGenre: '#select_genre',
-//         selectPlaylist: '#select_playlist',
-//         buttonSubmit: '#btn_submit',
-//         divSongDetail: '#song-detail',
-//         hfToken: '#hidden_token',
-//         divSonglist: '.song-list'
-//     }
-
-//     //public methods
-//     return {
-
-//         //method to get input fields
-//         inputField() {
-//             return {
-//                 genre: document.querySelector(DOMElements.selectGenre),
-//                 playlist: document.querySelector(DOMElements.selectPlaylist),
-//                 tracks: document.querySelector(DOMElements.divSonglist),
-//                 submit: document.querySelector(DOMElements.buttonSubmit),
-//                 songDetail: document.querySelector(DOMElements.divSongDetail)
-//             }
-//         },
-
-//         // need methods to create select list option
-//         createGenre(text, value) {
-//             const html = `<option value="${value}">${text}</option>`;
-//             document.querySelector(DOMElements.selectGenre).insertAdjacentHTML('beforeend', html);
-//         },
-
-//         createPlaylist(text, value) {
-//             const html = `<option value="${value}">${text}</option>`;
-//             document.querySelector(DOMElements.selectPlaylist).insertAdjacentHTML('beforeend', html);
-//         },
-
-//         // need method to create a track list group item 
-//         createTrack(id, name) {
-//             const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`;
-//             document.querySelector(DOMElements.divSonglist).insertAdjacentHTML('beforeend', html);
-//         },
-
-//         // need method to create the song detail
-//         createTrackDetail(img, title, artist) {
-
-//             const detailDiv = document.querySelector(DOMElements.divSongDetail);
-//             // any time user clicks a new song, we need to clear out the song detail div
-//             detailDiv.innerHTML = '';
-
-//             const html =
-//                 `
-//             <div class="row col-sm-12 px-0">
-//                 <img src="${img}" alt="">        
-//             </div>
-//             <div class="row col-sm-12 px-0">
-//                 <label for="Genre" class="form-label col-sm-12">${title}:</label>
-//             </div>
-//             <div class="row col-sm-12 px-0">
-//                 <label for="artist" class="form-label col-sm-12">By ${artist}:</label>
-//             </div> 
-//             `;
-
-//             detailDiv.insertAdjacentHTML('beforeend', html)
-//         },
-
-//         resetTrackDetail() {
-//             this.inputField().songDetail.innerHTML = '';
-//         },
-
-//         resetTracks() {
-//             this.inputField().tracks.innerHTML = '';
-//             this.resetTrackDetail();
-//         },
-
-//         resetPlaylist() {
-//             this.inputField().playlist.innerHTML = '';
-//             this.resetTracks();
-//         },
-
-//         storeToken(value) {
-//             document.querySelector(DOMElements.hfToken).value = value;
-//         },
-
-//         getStoredToken() {
-//             return {
-//                 token: document.querySelector(DOMElements.hfToken).value
-//             }
-//         }
-//     }
-
-// })();
-
-// const APPController = (function (UICtrl, APICtrl) {
-
-//     // get input field object ref
-//     const DOMInputs = UICtrl.inputField();
-
-//     // get genres on page load
-//     const loadGenres = async () => {
-//         //get the token
-//         const token = await APICtrl.getToken();
-//         //store the token onto the page
-//         UICtrl.storeToken(token);
-//         //get the genres
-//         const genres = await APICtrl.getGenres(token);
-//         //populate our genres select element
-//         genres.forEach(element => UICtrl.createGenre(element.name, element.id));
-//     }
-
-//     // create genre change event listener
-//     DOMInputs.genre.addEventListener('change', async () => {
-//         //reset the playlist
-//         UICtrl.resetPlaylist();
-//         //get the token that's stored on the page
-//         const token = UICtrl.getStoredToken().token;
-//         // get the genre select field
-//         const genreSelect = UICtrl.inputField().genre;
-//         // get the genre id associated with the selected genre
-//         const genreId = genreSelect.options[genreSelect.selectedIndex].value;
-//         // ge the playlist based on a genre
-//         const playlist = await APICtrl.getPlaylistByGenre(token, genreId);
-//         // create a playlist list item for every playlist returned
-//         playlist.forEach(p => UICtrl.createPlaylist(p.name, p.tracks.href));
-//     });
-
-
-//     // create submit button click event listener
-//     DOMInputs.submit.addEventListener('click', async (e) => {
-//         // prevent page reset
-//         e.preventDefault();
-//         // clear tracks
-//         UICtrl.resetTracks();
-//         //get the token
-//         const token = UICtrl.getStoredToken().token;
-//         // get the playlist field
-//         const playlistSelect = UICtrl.inputField().playlist;
-//         // get track endpoint based on the selected playlist
-//         const tracksEndPoint = playlistSelect.options[playlistSelect.selectedIndex].value;
-//         // get the list of tracks
-//         const tracks = await APICtrl.getTracks(token, tracksEndPoint);
-//         // create a track list item
-//         tracks.forEach(el => UICtrl.createTrack(el.track.href, el.track.name))
-
-//     });
-
-//     // create song selection click event listener
-//     DOMInputs.tracks.addEventListener('click', async (e) => {
-//         // prevent page reset
-//         e.preventDefault();
-//         UICtrl.resetTrackDetail();
-//         // get the token
-//         const token = UICtrl.getStoredToken().token;
-//         // get the track endpoint
-//         const trackEndpoint = e.target.id;
-//         //get the track object
-//         const track = await APICtrl.getTrack(token, trackEndpoint);
-//         // load the track details
-//         UICtrl.createTrackDetail(track.album.images[2].url, track.name, track.artists[0].name);
-//     });
-
-//     return {
-//         init() {
-//             console.log('App is starting');
-//             loadGenres();
-//         }
-//     }
-
-// })(UIController, APIController);
-
-// // will need to call a method to load the genres on page load
-// APPController.init();
-// /* Lidell's area */
-
-/* Jesus' area */
-
+/* LYRICS FETCH */
 function fetchLyrics() {
   var lyricsUrl = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=253912241&apikey=1bea274e43466310a83604d5c9dffd24"
 
@@ -474,7 +129,7 @@ function fetchLyrics() {
     .then((data) => displayModal(data))
 }
 
-/* Modal for handling various displays */
+/* DISPLAY OVERLAY MODAL */
 var modal = document.getElementById("myModal");
 var span = document.getElementsByClassName("close")[0];
 
@@ -494,41 +149,3 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
-
-/* SWIPER */
-/* RESPONSIVE SLIDER 
-var TrandingSlider = new Swiper('.tranding-slider', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  loop: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 100,
-    modifier: 2.5,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-});
-*/
-
-/* FROM SWIPER
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-*/
-/* Jesus' area */
