aa5c39446e24db97708effb03fc0d7d177aee617
Jesus Santos <jo_nez@hotmail.com>
2023-06-19 12:41:33 -0700

    slider and layout working and finished

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,12 @@
 /* GLOBAL VARIABLES */
 var apiKey = '1bea274e43466310a83604d5c9dffd24';
 var historyContainer = document.getElementById('history-container');
-var trackSearchDisplay = document.getElementById('main-container');
+var searchContainer = document.getElementById('search-container');
 var searchText = document.getElementById('search-text');
 var searchButton = document.getElementById('search-button');
-var sliderContainer = document.getElementById('results');
-var trackSearchResultsCardContainer = document.getElementById('results-card-container');
-var trackSearchResultsCardContainerPaginator = document.getElementById('results-paginator');
+var resultsContainer = document.getElementById('results-container');
+var slideContainer = document.getElementById('slide-container');
+var resultsPaginator = document.getElementById('results-paginator');
 
 var searches = localStorage.getItem('searches');
 searches = (searches) ? JSON.parse(searches) : [];
@@ -32,92 +32,103 @@ function performSearch(trackTitle) {
 };
 
 function displayTrackSearchResults(trackSearchData) {
-/* HIDE APP TITLE & SEARCH BAR */
-  trackSearchDisplay.style.display = 'none';
-/* CLEAR RESULTS CONTAINER */
-  trackSearchResultsCardContainer.innerHTML = "";
+  /* HIDE APP TITLE & SEARCH BAR */
+  searchContainer.style.display = 'none';
+  /* CLEAR RESULTS CONTAINER */
+  slideContainer.innerHTML = "";
 
-/* LOOP THROUGH RESULTS */
+  /* LOOP THROUGH RESULTS */
   for (let i = 0; i < trackSearchData.message.body.track_list.length; i++) {
-    var trackIndex = trackSearchData.message.body.track_list[i];
-    var trackId = trackIndex.track.track_id;
-    var trackName = trackIndex.track.track_name;
-    // var albumId = trackIndex.track.album_id;
-    var albumName = trackIndex.track.album_name;
-    // var artistId = trackIndex.track.artist_id;
-    var artistName = trackIndex.track.artist_name;
+    var resultsObject = trackSearchData.message.body.track_list[i];
+    var trackId = resultsObject.track.track_id;
+    var trackName = resultsObject.track.track_name;
+    var albumId = resultsObject.track.album_id;
+    var albumName = resultsObject.track.album_name;
+    var artistId = resultsObject.track.artist_id;
+    var artistName = resultsObject.track.artist_name;
 
-    var trackSearchResultsItem = document.createElement('div');
-    trackSearchResultsItem.setAttribute('id', trackId + '-container');
-    trackSearchResultsItem.setAttribute('class', 'card-user-container swiper-slide');
+    /* SLIDE */
+    var slide = document.createElement('div');
+    slide.setAttribute('id', artistId + '-' + albumId + '-' + trackId);
+    slide.setAttribute('class', 'swiper-slide result-slide');
 
-    var trackAlbumArtContainer = document.createElement('div');
-    trackAlbumArtContainer.setAttribute('id', trackId + '-art-container');
-    trackAlbumArtContainer.setAttribute('class', 'card-user-avatar');
+    /* ALBUM ART CONTAINER */
+    var albumArtContainer = document.createElement('div');
+    albumArtContainer.setAttribute('id', trackId + '-album-art');
+    albumArtContainer.setAttribute('class', 'result-slide-img');
 
-    var trackAlbumArt = document.createElement('img');
-    trackAlbumArt.setAttribute('id', trackId + '-album-art');
-    trackAlbumArt.setAttribute('src', 'https://via.placeholder.com/30x30');
-    trackAlbumArt.setAttribute('alt', albumName + ' cover art.');
-    trackAlbumArt.setAttribute('class', 'user-image');
+    /* ALBUM ART */
+    var albumArt = document.createElement('img');
+    albumArt.setAttribute('src', 'https://i.scdn.co/image/ab67616d0000b2738399047ff71200928f5b6508');
+    albumArt.setAttribute('alt', artistName + ' - ' + albumName + ' cover art.');
 
-    trackAlbumArtContainer.appendChild(trackAlbumArt);
-    
-    var trackLinksContainer = document.createElement('div');
-    trackLinksContainer.setAttribute('id', trackId + '-album-links');
-    trackLinksContainer.setAttribute('class', 'card-user-social');
+    /* APPEND ALBUM ART TO ALBUM ART CONTAINER */
+    albumArtContainer.appendChild(albumArt);
 
-    var trackLinksListContainer = document.createElement('ul');
-    trackLinksListContainer.setAttribute('id', trackId + '-album-links-list');
-    trackLinksListContainer.setAttribute('class', 'menu');
+    /* APPEND ALBUM ART CONTAINER TO slide CARD */
+    slide.appendChild(albumArtContainer);
 
-    var trackLinksListLyrics = document.createElement('li');
-    trackLinksListLyrics.setAttribute('id', trackId + '-track-lyrics');
-    trackLinksListLyrics.setAttribute('class', 'fa-solid fa-microphone-lines fa-2xl');
-    
-    var trackLinksListWatch = document.createElement('li');
-    trackLinksListWatch.setAttribute('id', trackId + '-track-watch');
-    trackLinksListWatch.setAttribute('class', 'fa-brands fa-youtube fa-2xl');
+    /* INFO CONTAINER */
+    var infoContainer = document.createElement('div');
+    infoContainer.setAttribute('class', 'result-slide-content');
 
-    var trackLinksListListen = document.createElement('li');
-    trackLinksListListen.setAttribute('id', trackId + '-track-listen');
-    trackLinksListListen.setAttribute('class', 'fa-brands fa-spotify fa-2xl');
+    /* ARTIST */
+    var artist = document.createElement('h1');
+    artist.setAttribute('class', 'album-artist slide-text');
+    artist.textContent = artistName;
 
-    var trackLinksListBuy = document.createElement('li');
-    trackLinksListBuy.setAttribute('id', trackId + '-track-buy');
-    trackLinksListBuy.setAttribute('class', 'fa-brands fa-amazon fa-2xl');
+    /* APPEND ARTIST TO INFO CONTAINER */
+    infoContainer.appendChild(artist);
 
-    trackLinksListContainer.appendChild(trackLinksListLyrics);
-    trackLinksListContainer.appendChild(trackLinksListWatch);
-    trackLinksListContainer.appendChild(trackLinksListListen);
-    trackLinksListContainer.appendChild(trackLinksListBuy);
-    trackLinksContainer.appendChild(trackLinksListContainer);
-    
-    var trackTitlesContainer = document.createElement('div');
-    trackTitlesContainer.setAttribute('id', trackId + '-track-info-container');
-    trackTitlesContainer.setAttribute('class', 'card-user-bio');
-    
-    var trackTitlesArtist = document.createElement('h4');
-    trackTitlesArtist.setAttribute('id', trackId + '-track-artist');
-    trackTitlesArtist.setAttribute('class', 'track-artist');
-    trackTitlesArtist.textContent = artistName;
-    
-    var trackTitlesTrack = document.createElement('p');
-    trackTitlesTrack.setAttribute('id', trackId + '-track-title');
-    trackTitlesTrack.setAttribute('class', 'track-title');
-    trackTitlesTrack.textContent = trackName;
-    
-    trackTitlesContainer.appendChild(trackTitlesArtist);
-    trackTitlesContainer.appendChild(trackTitlesTrack);
+    /* LINKS CONTAINER */
+    var linksContainer = document.createElement('div');
+    linksContainer.setAttribute('class', 'result-slide-content-bottom');
 
-    trackSearchResultsItem.appendChild(trackTitlesContainer);
-    trackSearchResultsItem.appendChild(trackAlbumArtContainer);
-    trackSearchResultsItem.appendChild(trackLinksContainer);
-    
-    trackSearchResultsCardContainer.appendChild(trackSearchResultsItem);
+    /* TITLE */
+    var title = document.createElement('h2');
+    title.setAttribute('class', 'track-title slide-text');
+    title.textContent = trackName;
+
+    /* APPEND TITLE TO INFO CONTAINER */
+    linksContainer.appendChild(title);
+
+    /* LINK LIST */
+    var linkList = document.createElement('ul');
+    linkList.setAttribute('id', trackId);
+    linkList.setAttribute('class', 'option');
+
+    /* LINK LIST ITEMS*/
+    var lyrics = document.createElement('li');
+    lyrics.setAttribute('id',trackId + ' lyrics');
+    lyrics.setAttribute('class', 'fa-solid fa-microphone-lines');
+
+    var video = document.createElement('li');
+    video.setAttribute('id',trackId + ' video');
+    video.setAttribute('class', 'fa-brands fa-youtube');
+
+    var listen = document.createElement('li');
+    listen.setAttribute('id',trackId + ' listen');
+    listen.setAttribute('class', 'fa-brands fa-spotify');
+
+    var buy = document.createElement('li');
+    buy.setAttribute('id',trackId + ' buy');
+    buy.setAttribute('class', 'fa-brands fa-amazon');
+
+    /* APPEND ITEMS TO LIST */
+    linkList.appendChild(lyrics);
+    linkList.appendChild(video);
+    linkList.appendChild(listen);
+    linkList.appendChild(buy);
+
+    /* APPEND TO INFO CONTAINER  */
+    linksContainer.appendChild(linkList);
+    infoContainer.appendChild(linksContainer);
+    slide.appendChild(infoContainer);
+    slideContainer.appendChild(slide);
+
   }
 
-  sliderContainer.style.display = 'block';
+  resultsContainer.style.display = 'block';
 }
 
 /* LYRICS FETCH */
@@ -149,3 +160,32 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+
+function querySpotify() {
+  const clientId = '07ff8b4538b94722848066ac1547ceb1';
+  const clientSecret = 'e29634d15e1d4ac4bf533209bcc77367';
+
+}
+
+var ResultsSlider = new Swiper('.results-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
